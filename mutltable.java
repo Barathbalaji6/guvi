6367b161be7a6d99927600970f8c56b9e6d9945f
Barathbalaji6 <barathbalaji6@gmail.com>
2017-07-10 14:48:56 +0530

    Create mutltable.java

---
+++ mutltable.java
@@ -0,0 +1,19 @@
+package aa;
+
+import java.util.Scanner;
+public class Multiplication {
+
+      public static void main(String[] args) {
+        
+        int n;
+        System.out.println("enter a number");
+        @SuppressWarnings("resource")
+		Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+"*"+i+"="+n*i);
+        }
+    }
+    
+}
