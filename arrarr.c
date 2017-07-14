ab690f5249797c8b95aa0e4ba5b7de4b061ddb4c
Barathbalaji6 <barathbalaji6@gmail.com>
2017-07-14 14:14:00 +0530

    Create arrarr.c

---
+++ arrarr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+	int a[12],b[12],sum[12];
+	int k;
+  printf("\n Enter the  a:");
+  for(k=0;k<12;k++)
+  scanf("%d",&a[k]);
+   printf("\n Enter the b:");
+  for(k=0;k<12;k++)
+  scanf("%d",&b[i]);
+   for(k=0;k<12;k++)
+ sum[k]=a[k]+b[k];
+ printf("\n Sum of array is :");
+ for(k=0;k<12;k++)
+ printf("\t%d",sum[k]);
+ 	return 0;
+}
