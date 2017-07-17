0e437f8c08e4bdffa8400abd8b29e565fffcef87
Barathbalaji6 <barathbalaji6@gmail.com>
2017-07-17 10:45:50 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,16 @@
+package aa;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a,b,c,i;
+		a=50;
+		b=10;
+	    c=b;
+		for(i=1;i<=a;i++){
+			c++;
+		}System.out.println(c);
+
+	}
+
+}
