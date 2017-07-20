5df0b455f045f30b01c1d711fe9738aab1b5802f
Barathbalaji6 <barathbalaji6@gmail.com>
2017-07-20 11:43:32 +0530

    Create checksortedarray.java

---
+++ checksortedarray.java
@@ -0,0 +1,28 @@
+package aa;
+
+import java.util.*;
+class CheckSortedArray
+{
+	public static void main(String[] args)
+	{
+		@SuppressWarnings("resource")
+		Scanner s=new Scanner(System.in);
+		int p=0;
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+			{
+			a[i]=s.nextInt();
+			}
+		for(int i=0;i<n-1;i++)
+			{
+			if(a[i]>a[i+1])
+				 p=1;
+			}
+		
+		if(p==0)
+			System.out.println("true");
+		else
+			System.out.println("false");
+	}
+}
